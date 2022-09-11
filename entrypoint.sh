362cef4c1c66e29410997c28d3ca45d3944c483a
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-09-11 11:09:43 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
-
+mkdir -p ${DIR_CONFIG}
 # Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+cat << EOF > ${DIR_CONFIG}/heroku.json
 {
     "inbounds": [{
         "port": ${PORT},
@@ -35,12 +35,12 @@ curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.co
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+
+#${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/v2ray run -c ${DIR_CONFIG}/heroku.json
