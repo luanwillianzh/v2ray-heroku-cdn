4ea0f3a169d75b5a767df0b430192bfb84377c3e
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-09-11 10:33:39 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,23 @@
 DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
-
+mkdir -p ${DIR_CONFIG}
 # Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+cat << EOF > ${DIR_CONFIG}/heroku.json
 {
     "inbounds": [{
-        "port": ${PORT},
+        "port": 80,
         "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "${ID}",
+                "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
                 "alterId": 0
             }]
         },
         "streamSettings": {
             "network": "ws",
             "wsSettings": {
-                "path": "${WSPATH}"
+                "path": "/"
             }
         }
     }],
@@ -35,12 +35,12 @@ curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.co
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+
+#${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/v2ray run -c ${DIR_CONFIG}/heroku.json
